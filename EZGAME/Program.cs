d3caf8a3f5f8fce4f9f8cd12fe9e9947736b61a2
Monticelli Romain <rmm.monticelli@gmail.com>
2019-06-18 10:52:03 +0200

    game first version EZ

--- EZGAME/Program.cs
+++ EZGAME/Program.cs
@@ -7,7 +7,105 @@ namespace EZGAME
     {
         public static void Main(string[] args)
         {
+            string classPlayer1;
+            string classPlayer2;
 
+            do 
+            {
+                Console.WriteLine(" Player 1, choose your class (magician - warrior)");
+                classPlayer1 = Console.ReadLine();
+            }
+            while (classPlayer1 != "magician" && classPlayer1 != "warrior");
+
+            do
+            {
+                Console.WriteLine("\nPlayer 2, choose your class (magician - warrior)");
+                classPlayer2 = Console.ReadLine();
+            }
+            while (classPlayer2 != "magician" && classPlayer2 != "warrior");
+
+            Character player1;
+            Character player2;
+
+                if (classPlayer1 == "magician")
+            {
+                player1 = new Magician();
+            }
+
+            else
+            {
+                player1 = new Warrior();
+            }
+
+            if (classPlayer2 == "magician")
+            {
+                player2 = new Magician();
+            }
+
+            else
+            {
+                player2 = new Warrior();
+            }
+
+            int playerTurn = 0;
+            bool gameOver = true;
+            while (gameOver)
+            {
+                Character ToPlay;
+                Character ToHit;
+
+                if (playerTurn == 0)
+                {
+                    ToPlay = player1;
+                    ToHit = player2;
+                }
+
+                else
+                {
+                    ToPlay = player2;
+                    ToHit = player1;
+                }
+
+                Console.WriteLine("\n" + ToPlay.Hello());
+                Console.WriteLine("Player skills: ");
+                for (int i = 0; i < ToPlay.skills.Count; i++)
+                {
+                    Console.WriteLine(ToPlay.skills[i].GetName() + " (mana cost: " + ToPlay.skills[i].GetManaUsed() + ")");
+                }
+
+                Console.WriteLine("Vie: " + ToPlay.vie);
+                Console.WriteLine("Mana: " + ToPlay.mana);
+                bool isSkillUsed = false;
+                while (!isSkillUsed) 
+                {
+                    Console.WriteLine("Use of your skill\n");
+                    string skillUsed = Console.ReadLine();
+                    for(int i = 0; i < ToPlay.skills.Count; i++)
+                    {
+                        if (skillUsed == ToPlay.skills[i].GetName())
+                        {
+                            int manaLeft = ToPlay.mana - ToPlay.skills[i].GetManaUsed();
+                            if (manaLeft >= 0)
+                            {
+                                ToPlay.mana = manaLeft;
+                                ToPlay.skills[i].RunSkill(ToHit);
+                                isSkillUsed = true;
+                                Console.Write("\n" + ToHit.OnHit());
+                                playerTurn = (playerTurn + 1) % 2;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Not enough mana for this skill");
+                            }
+                        }
+                    }
+                }
+
+                if(ToHit.vie <= 0)
+                {
+                    gameOver = false;
+                }
+            }
         }
     }
 
@@ -26,9 +124,9 @@ namespace EZGAME
             return 10;
         }
 
-        public void RunSkill(Character hit)
+        public void RunSkill(Character characterHit)
         {
-
+            characterHit.vie -= 10;
         }
 
         public string GetName()
@@ -44,14 +142,14 @@ namespace EZGAME
             return 15;
         }
 
-        public void RunSkill(Character hit)
+        public void RunSkill(Character characterHit)
         {
-
+            characterHit.vie -= 17;
         }
 
         public string GetName()
         {
-            return "Black Hole";
+            return "Barronage";
         }
     }
 
@@ -63,9 +161,9 @@ namespace EZGAME
             return 20;
         }
 
-        public void RunSkill(Character hit)
+        public void RunSkill(Character characterHit)
         {
-
+            characterHit.vie -= 20;
         }
 
         public string GetName()
@@ -78,12 +176,12 @@ namespace EZGAME
     {
         public int GetManaUsed()
         {
-            return 30;
+            return 35;
         }
 
-        public void RunSkill(Character hit)
+        public void RunSkill(Character characterHit)
         {
-
+            characterHit.vie -= 35;
         }
 
         public string GetName()
@@ -94,56 +192,55 @@ namespace EZGAME
 
     abstract class Character 
     {
-        List <Skill> skills;
-        int vie;
-        int mana;
-        string onHit() { return "il m'a frappé omg"; }
-        string hello() { return "salut bitch"; }
+        public List<Skill> skills;
+        public int vie;
+        public int mana;
+        abstract public string OnHit();
+        abstract public string Hello();
 
     }
     class Warrior : Character
     {
-        List <Skill> skills;
-
-        int vie = 100;
-        int mana = 50;
 
         public Warrior()
         {
+            skills = new List<Skill>();
             skills.Add(new Spin_To_Win());
             skills.Add(new Barronage());
+
+            vie = 100;
+            mana = 60;
         }
 
-        public string onHit() 
+        public override string OnHit() 
         { 
             return "You can't milk those"; 
         }
 
-        public string hello() 
+        public override string Hello() 
         { 
             return "Salut moucheron"; 
         }
     }
     class Magician : Character
     {
-
-        List <Skill> skills = new List<Skill> ();
-
-        int vie = 80;
-        int mana = 100;
-
+       
         public Magician()
         {
+            skills = new List<Skill>();
             skills.Add(new Black_Hole());
             skills.Add(new Hellfire());
+
+            vie = 80;
+            mana = 100;
         }
 
-        public string onHit()
+        public override string OnHit()
         {
             return "You shall hit me from afar";
         }
 
-        public string hello()
+        public override string Hello()
         {
             return "Prepare your anus for my scepter";
         }
