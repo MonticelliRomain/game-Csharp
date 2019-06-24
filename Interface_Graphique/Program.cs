7a2af81418e1aef4947bf705db1337fc4abced62
Monticelli Romain <rmm.monticelli@gmail.com>
2019-06-24 10:12:52 +0200

    basic graphic interface

---
+++ Interface_Graphique/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+
+namespace Interface_Graphique
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            /*MainWindow win = new MainWindow();
+            win.Show();*/
+            //Create the Window
+            Window myWin = new Window("My first GTK# Application! ");
+            myWin.Resize(200, 200);
+
+            //Create a label and put some text in it.
+            Label myLabel = new Label();
+            myLabel.Text = "Hello World!!!!";
+
+            //Add the label to the form
+            myWin.Add(myLabel);
+
+            //Show Everything
+            myWin.ShowAll();
+            Application.Run();
+        }
+    }
+}
